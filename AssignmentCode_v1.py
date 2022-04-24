693d60f97bedfd0cfe459410a9913bd4866de815
gy21cdl <102544878+gy21cdl@users.noreply.github.com>
2022-04-24 11:40:54 +0100

    Update AssignmentCode_v1.py

--- AssignmentCode_v1.py
+++ AssignmentCode_v1.py
@@ -5,9 +5,16 @@ Created on Sun Apr 24 10:51:27 2022
 @author: Chris Lambert
 """
 
+#Imported modules
 import pandas as pd
 
-y  = {'f(x)': [2,4,6,8]}
+#open csv file using pandas
+#create a data fame variable (df)
+df = pd.read_csv (r'D:\UniversityOfLeeds\Year1\GEOG5003M_ProgrammingForGeographicalInformationAnalysis\Assignment2_Code\Assignment2_RawData_v2.csv')   
+#print (df) #print check
+
+
+y  = {'f(x)': [2,4,6,8], 'g(x)':[4,2,-2,6]}
 x = [1,2,3,4]
 
 graph = pd.DataFrame(y,x)
